248d46010cca69dd2f9988ecf92dd09729da8185
ChristopheBelpaire <christophe.belpaire@selinko.com>
2023-12-31 12:57:45 +0100

    Test with fix from : git@github.com:SteffenDE/phoenix_live_view.git branch : infinite_scroll_fix_assets

--- mix.exs
+++ mix.exs
@@ -35,7 +35,7 @@ defmodule PhxViewportBottomBug.MixProject do
       {:phoenix, "~> 1.7.7"},
       {:phoenix_html, "~> 3.3"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 0.19.0"},
+      {:phoenix_live_view, git: "git@github.com:SteffenDE/phoenix_live_view.git", branch: "infinite_scroll_fix_assets", override: true},
       {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.8.0"},
       {:esbuild, "~> 0.7", runtime: Mix.env() == :dev},
